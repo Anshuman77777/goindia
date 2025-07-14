c4cedb32b09b165d0b18398074969b4d867b56ec
Anshuman <sanshuman4422@gmail.com>
2025-07-14 21:50:14 +0530

    deleted client

--- supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import {createClient} from '@supabase/supabase-js' 
-const projectUrl=import.meta.env.VITE_SUPABASE_PROJECT_URL;
-const anonKey=import.meta.env.VITE_SUPABASE_ANON_KEY;
-  const supabase = createClient(projectUrl,anonKey);
-  export default supabase;
